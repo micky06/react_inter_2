1ae6826ef7a1de6599825e89898e68ac590120b1
michael <m-michaux@outlook.fr>
2022-02-09 21:56:45 +0100

    nettoyage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [quote, setQuote] = useState('');
+  const [clickPair, setClickPair] = useState(true);
+
+  useEffect(() => {
+
+    axios
+    .get('https://simpsons-quotes-api.herokuapp.com/quotes')
+    .then(response => {
+      setQuote(response.data[0])
+    })
+    
+    
+  },[clickPair])
+  console.log(quote.quote)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className='App__header'>
+        <div className='App-logo'>
+         <img src={quote.image} alt={quote.character}/>
+        </div>
+        <div className='App__card-character'>
+          <p>{quote.character}</p>
+        </div>
+        <div className='App__card-quote'>
+          <p>{quote.quote}</p>
+        </div>
+        <button onClick={() => setClickPair(!clickPair)} className='App-link'>New Quote</button>
+      </div>
     </div>
   );
 }
